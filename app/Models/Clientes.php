c987d876d557d3514ad32067a4b212f042dd58c9
Kario Freire <kario.freire@fabricainfo.com>
2022-09-26 13:07:24 -0300

    Model - Inicia modelo de clientes

---
+++ app/Models/Clientes.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Clientes extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = "clientes";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        "codigo_cliente",
+        "nome",
+        "cpf",
+        "sexo",
+        "email"
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        "created_at",
+        "updated_at"
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        "created_at" => "datetime:d/m/Y H:i"
+    ];
+}
