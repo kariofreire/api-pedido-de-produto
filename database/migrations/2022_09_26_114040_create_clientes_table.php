dad2b24472736ca01d1cf55742955aa687e66fdf
Kario Freire <kario.freire@fabricainfo.com>
2022-09-26 11:53:25 -0300

    Database - Inicia migration da tabela de clientes.

---
+++ database/migrations/2022_09_26_114040_create_clientes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+
+            $table->string("codigo_cliente", 8)->comment("Código do cliente.");
+            $table->string("nome")->comment("Nome do cliente.");
+            $table->string("cpf", 11)->comment("Cadastro de Pessoa Física do cliente.");
+            $table->enum("sexo", ["masculino", "feminino"])->comment("Sexo do cliente, lembrando que, sexo está relacionado às distinções anatômicas e biológicas entre homens e mulheres.");
+            $table->string("email")->comment("Email do cliente");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
