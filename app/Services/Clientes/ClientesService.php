4f24dc6bff67d719cbc40cf33c312e70a657b984
Kario Freire <kario.freire@fabricainfo.com>
2022-09-26 14:27:45 -0300

    Service - Implementa metodo index ao service de clientes

--- app/Services/Clientes/ClientesService.php
+++ app/Services/Clientes/ClientesService.php
@@ -3,6 +3,7 @@
 namespace App\Services\Clientes;
 
 use App\Repositories\Contracts\ClientesRepositoryInterface;
+use Illuminate\Http\Request;
 
 class ClientesService
 {
@@ -17,5 +18,21 @@ class ClientesService
 	public function __construct(ClientesRepositoryInterface $clientesRepository)
 	{
 		$this->repository = $clientesRepository;
+	}
+
+	/**
+	 * Retorna lista de clientes por filtro.
+	 * 
+	 * @param Request $request
+	 * 
+	 * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+	 */
+	public function getAllFilter(Request $request)
+	{
+		try {
+			return $this->repository->allFilter($request);
+		} catch (\Throwable $th) {
+			throw $th;
+		}
 	}
 }
