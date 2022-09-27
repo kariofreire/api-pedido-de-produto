2a4b0c601cd3eeb0c5a54b593595aeb2807d0c7a
Kario Freire <kariofreiretech@gmail.com>
2022-09-27 18:35:30 -0300

    Pedidos - Implementa metodo index

---
+++ app/Http/Controllers/Api/v1/ApiPedidosController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Helpers\ReturnResponse;
+use App\Http\Controllers\Controller;
+use App\Services\Pedidos\PedidosService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class ApiPedidosController extends Controller
+{
+    /** @var PedidosService $service */
+    protected PedidosService $service;
+
+    /**
+     * Define o Service utilizado neste Controller.
+     *
+     * @param PedidosService $pedidosService
+     *
+     * @return Void
+     */
+    public function __construct(PedidosService $pedidosService)
+    {
+        $this->service = $pedidosService;
+    }
+
+    /**
+     * Retorna todos os pedidos.
+     *
+     * @param Request $request
+     *
+     * @return JsonResponse
+     */
+    public function index(Request $request) : JsonResponse
+    {
+        try {
+            $dados = $this->service->getAllFilter($request);
+
+            return ReturnResponse::success("Dados retornados com sucesso.", $dados);
+        } catch (\Exception $e) {
+            return ReturnResponse::error("Não foi possível retornar os dados.", ["erro" => $e->getMessage()]);
+        }
+    }
+}
--- app/Services/Pedidos/PedidosService.php
+++ app/Services/Pedidos/PedidosService.php
@@ -3,6 +3,7 @@
 namespace App\Services\Pedidos;
 
 use App\Repositories\Contracts\PedidosRepositoryInterface;
+use Illuminate\Http\Request;
 
 class PedidosService
 {
@@ -17,5 +18,21 @@ class PedidosService
 	public function __construct(PedidosRepositoryInterface $pedidosRepository)
 	{
 		$this->repository = $pedidosRepository;
+	}
+
+    /**
+	 * Retorna lista de pedidos por filtro.
+	 *
+	 * @param Request $request
+	 *
+	 * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+	 */
+	public function getAllFilter(Request $request)
+	{
+		try {
+			return $this->repository->allFilter($request);
+		} catch (\Throwable $th) {
+			throw $th;
+		}
 	}
 }
